1605b1ffe8a1883c234044274785be8f2ea26b1a
gabrielius pocevicius <poceviciusgabrielius@gmail.com>
2022-01-29 18:16:09 +0200

    0.3

---
+++ Certificates.js
@@ -0,0 +1,32 @@
+//count certs as promise resolved
+let counter = 0
+
+async function cert(cid = '', path = '') {
+    let uuid = crypto.randomUUID()
+    let template = `
+    <tr id="${uuid}">
+        <embed class="pdf item" src="${ path }"  height="275">
+    </tr>
+    `
+    document.getElementById('cert-app').innerHTML += template
+    return;
+}
+
+
+
+cert('', 'data/UdacityCertificate.pdf')
+cert('', 'data/CertificateOfCompletion_Advanced Node.js.pdf')
+
+cert('', 'data/CertificateOfCompletion_CSS Essential Training.pdf')
+cert('', 'data/CertificateOfCompletion_EndtoEnd JavaScript Testing with Cypress.io.pdf')
+cert('', 'data/CertificateOfCompletion_Ethical Hacking with JavaScript.pdf')
+cert('', 'data/CertificateOfCompletion_JavaScript Best Practices for Code Formatting.pdf')
+cert('', 'data/CertificateOfCompletion_JavaScript Best Practices for Data.pdf')
+cert('', 'data/CertificateOfCompletion_JavaScript Security Essentials.pdf')
+cert('', 'data/CertificateOfCompletion_JavaScript TestDriven Development ES6.pdf')
+cert('', 'data/CertificateOfCompletion_Learning MongoDB.pdf')
+cert('', 'data/CertificateOfCompletion_Master JavaScript.pdf')
+cert('', 'data/CertificateOfCompletion_Node.js Design Patterns.pdf')
+cert('', 'data/CertificateOfCompletion_Responsive Layout.pdf')
+cert('', 'data/Gabrielius Pocevicius_JavaScript.pdf')
+cert('', 'data/Gabrielius Pocevicius_Python.pdf')
---
+++ animate.js
@@ -0,0 +1,23 @@
+let boot = gsap.timeline({ defaults: { ease: "power1.out" } });
+boot.set('div', { autoAlpha: 0, })
+    /* setTimeout(() => { */
+boot.to('div', { y: 0, autoAlpha: 1, duration: 3 })
+    /* }, 0) */
+
+
+
+let tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+
+
+tl.to('.item', { autoAlpha: 0, y: 100 })
+
+tl.to('.item', { autoAlpha: 0.5, y: -15, stagger: 0.059, duration: 0.8 }, "+=4")
+
+tl.to('.item', { y: 0, autoAlpha: 1, stagger: 0.1, duration: 1 }, "-=1")
+
+
+
+
+tl.from(".animate-link-effect", { autoAlpha: 0.8, y: 12, repeat: 'true' })
+tl.to(".animate-link-effect", { autoAlpha: 1, y: 0, stagger: 0.059, duration: 1, repeat: 'true' })
