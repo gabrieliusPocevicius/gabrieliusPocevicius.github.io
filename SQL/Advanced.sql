6605da6077a6825b6d352d13ea97b1831ccbe1a8
gabrielius pocevicius <poceviciusgabrielius@gmail.com>
2022-06-08 19:51:16 +0300

    fades

---
+++ SQL/Advanced.sql
@@ -0,0 +1,8 @@
+--efficiently accessing data and maintaining optimal performance.
+
+/* 
+    Use different database data access patterns
+    Efficiently access data
+    Understand data performance optimization practices
+    Use software to track database performance
+ */
